200edf712b59105f29bd60d67bdf624c80dff485
Ryan Dabler <rpdabler@gmail.com>
2023-01-21 13:14:02 -0700

    feat: add beginning stage of static analyzer

---
+++ static_analyzer/static_analyzer.go
@@ -0,0 +1,62 @@
+package staticanalyzer
+
+import (
+	"calabash/ast"
+	"calabash/internal/visitor"
+)
+
+type analyzer struct{}
+
+func (a *analyzer) Analyze(ast []ast.Node) error {
+	for _, n := range ast {
+		err := a.analyzeNode(n)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *analyzer) analyzeNode(n ast.Node) error {
+	_, err := visitor.Accept[interface{}](n, a)
+
+	return err
+}
+
+func (a *analyzer) VisitBinaryExpr(e ast.BinaryExpr) (interface{}, error) {
+	err := a.analyzeNode(e.Left)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = a.analyzeNode(e.Right)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (a *analyzer) VisitGroupingExpr(e ast.GroupingExpr) (interface{}, error) {
+	return nil, a.analyzeNode(e.Expr)
+}
+
+func (a *analyzer) VisitNumLitExpr(e ast.NumericLiteralExpr) (interface{}, error) {
+	return nil, nil
+}
+
+func (a *analyzer) VisitStrLitExpr(e ast.StringLiteralExpr) (interface{}, error) {
+	return nil, nil
+}
+
+func (a *analyzer) VisitUnaryExpr(e ast.UnaryExpr) (interface{}, error) {
+	return nil, a.analyzeNode(e.Expr)
+}
+
+func New() *analyzer {
+	return &analyzer{}
+}
