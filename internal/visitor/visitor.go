78086a486159136920f85afc82dc1169581e99c0
Ryan Dabler <rpdabler@gmail.com>
2023-04-28 20:29:08 -0600

    refactor: Accept func now uses type switching instead of if statements
    Closes issue #5

--- internal/visitor/visitor.go
+++ internal/visitor/visitor.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 )
 
-type visitor[T any] interface {
+type evisitor[T any] interface {
 	VisitBinaryExpr(e ast.BinaryExpr) (T, error)
 	VisitUnaryExpr(e ast.UnaryExpr) (T, error)
 	VisitGroupingExpr(e ast.GroupingExpr) (T, error)
@@ -15,6 +15,10 @@ type visitor[T any] interface {
 	VisitBooleanLitExpr(e ast.BooleanLiteralExpr) (T, error)
 	VisitIdentifierExpr(e ast.IdentifierExpr) (T, error)
 	VisitFuncExpr(e ast.FuncExpr) (T, error)
+	VisitCallExpr(e ast.CallExpr) (T, error)
+}
+
+type svisitor[T any] interface {
 	VisitVarDeclStmt(s ast.VarDeclStmt) (T, error)
 	VisitAssignStmt(s ast.AssignmentStmt) (T, error)
 	VisitIfStmt(s ast.IfStmt) (T, error)
@@ -22,63 +26,108 @@ type visitor[T any] interface {
 	VisitRetStmt(s ast.ReturnStmt) (T, error)
 }
 
+type visitor[T any] interface {
+	evisitor[T]
+	svisitor[T]
+}
+
+func AcceptExpr[T any](e ast.Expr, v evisitor[T]) (T, error) {
+	var empty T
+
+	switch e.(type) {
+	case ast.BinaryExpr:
+		e := e.(ast.BinaryExpr)
+
+		return v.VisitBinaryExpr(e)
+
+	case ast.UnaryExpr:
+		e := e.(ast.UnaryExpr)
+
+		return v.VisitUnaryExpr(e)
+
+	case ast.GroupingExpr:
+		e := e.(ast.GroupingExpr)
+
+		return v.VisitGroupingExpr(e)
+
+	case ast.NumericLiteralExpr:
+		e := e.(ast.NumericLiteralExpr)
+
+		return v.VisitNumLitExpr(e)
+
+	case ast.StringLiteralExpr:
+		e := e.(ast.StringLiteralExpr)
+
+		return v.VisitStrLitExpr(e)
+
+	case ast.BottomLiteralExpr:
+		e := e.(ast.BottomLiteralExpr)
+
+		return v.VisitBottomLitExpr(e)
+
+	case ast.BooleanLiteralExpr:
+		e := e.(ast.BooleanLiteralExpr)
+
+		return v.VisitBooleanLitExpr(e)
+
+	case ast.IdentifierExpr:
+		e := e.(ast.IdentifierExpr)
+
+		return v.VisitIdentifierExpr(e)
+
+	case ast.FuncExpr:
+		e := e.(ast.FuncExpr)
+
+		return v.VisitFuncExpr(e)
+
+	case ast.CallExpr:
+		e, ok := e.(ast.CallExpr)
+
+		if !ok {
+			return empty, errors.New("Count not convert to call expr")
+		}
+
+		return v.VisitCallExpr(e)
+
+	}
+
+	return empty, errors.New("Unexpected expression")
+}
+
 func Accept[T any](n ast.Node, v visitor[T]) (T, error) {
 	var empty T
 
-	if n, ok := n.(ast.BinaryExpr); ok {
-		return v.VisitBinaryExpr(n)
+	e, ok := n.(ast.Expr)
+
+	if ok {
+		return AcceptExpr[T](e, v)
 	}
 
-	if n, ok := n.(ast.UnaryExpr); ok {
-		return v.VisitUnaryExpr(n)
-	}
+	switch n.(type) {
+	case ast.VarDeclStmt:
+		s := n.(ast.VarDeclStmt)
 
-	if n, ok := n.(ast.GroupingExpr); ok {
-		return v.VisitGroupingExpr(n)
-	}
+		return v.VisitVarDeclStmt(s)
 
-	if n, ok := n.(ast.NumericLiteralExpr); ok {
-		return v.VisitNumLitExpr(n)
-	}
+	case ast.AssignmentStmt:
+		s := n.(ast.AssignmentStmt)
 
-	if n, ok := n.(ast.StringLiteralExpr); ok {
-		return v.VisitStrLitExpr(n)
-	}
+		return v.VisitAssignStmt(s)
 
-	if n, ok := n.(ast.BottomLiteralExpr); ok {
-		return v.VisitBottomLitExpr(n)
-	}
+	case ast.IfStmt:
+		s := n.(ast.IfStmt)
 
-	if n, ok := n.(ast.BooleanLiteralExpr); ok {
-		return v.VisitBooleanLitExpr(n)
-	}
+		return v.VisitIfStmt(s)
 
-	if n, ok := n.(ast.IdentifierExpr); ok {
-		return v.VisitIdentifierExpr(n)
-	}
+	case ast.Block:
+		s := n.(ast.Block)
 
-	if n, ok := n.(ast.FuncExpr); ok {
-		return v.VisitFuncExpr(n)
-	}
+		return v.VisitBlock(s)
 
-	if n, ok := n.(ast.VarDeclStmt); ok {
-		return v.VisitVarDeclStmt(n)
-	}
+	case ast.ReturnStmt:
+		s := n.(ast.ReturnStmt)
 
-	if n, ok := n.(ast.AssignmentStmt); ok {
-		return v.VisitAssignStmt(n)
-	}
-
-	if n, ok := n.(ast.IfStmt); ok {
-		return v.VisitIfStmt(n)
-	}
-
-	if n, ok := n.(ast.Block); ok {
-		return v.VisitBlock(n)
-	}
-
-	if n, ok := n.(ast.ReturnStmt); ok {
-		return v.VisitRetStmt(n)
+		return v.VisitRetStmt(s)
 	}
 
 	return empty, errors.New("Supplied node did not match any node type")
